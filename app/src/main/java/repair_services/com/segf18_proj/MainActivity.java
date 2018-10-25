1c18d77908ba1c9309d98c9298b91c9188309aa1
Raj Khandekar <khandekarp@outlook.com>
2018-10-25 11:02:14 -0400

    admin account ifs

--- app/src/main/java/repair_services/com/segf18_proj/MainActivity.java
+++ app/src/main/java/repair_services/com/segf18_proj/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -15,50 +16,78 @@ import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.lang.String;
 
 
 public class MainActivity extends AppCompatActivity {
     private DatabaseReference accountsDatabase;
-    private EditText Name;
-    private EditText Password;
-    private Button Login;
-    private Button CreateNewAccount;
+    private EditText Name, Password, Email;
+    private Button Login, CreateNewUserAcc, CreateNewServiceProviderAcc, CreateAdminAcc;
+    String userrole;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        accountsDatabase = FirebaseDatabase.getInstance().getReference("accounts");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // create instance of database
+        accountsDatabase = FirebaseDatabase.getInstance().getReference("accounts");
 
+        //Input text fields
         Name = (EditText) findViewById(R.id.name);
         Password = (EditText) findViewById(R.id.password);
+        Email = (EditText) findViewById(R.id.email);
+
+        //Buttons
         Login = (Button) findViewById(R.id.LoginButton);
-        CreateNewAccount=(Button) findViewById(R.id.createAccount);
+        CreateNewUserAcc=(Button) findViewById(R.id.createUserAccount);
+        CreateNewServiceProviderAcc= (Button) findViewById(R.id.createServiceProviderAccount);
+        CreateAdminAcc = (Button) findViewById(R.id.createAdminAccount);
 
-        //login
+        //ON CLICK LISTENERS
+        //login button
         Login.setOnClickListener(new View.OnClickListener() {
-
             public void onClick(View view){
-                checkPassword(Name.getText().toString(),Password.getText().toString());
+                checkLogin(Name.getText().toString(),Email.getText().toString(), Password.getText().toString());
             }
         });
 
-        //create new account
-        CreateNewAccount.setOnClickListener(new View.OnClickListener() {
-
+        //create user account button
+        CreateNewUserAcc.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view){
-                Intent intent = new Intent(MainActivity.this, CreateNewAccount.class);
-                startActivity(intent);
+                checkLogin(Name.getText().toString(),Email.getText().toString(), Password.getText().toString());
+            }
+        });
+
+        //create service provider acc button
+        CreateNewServiceProviderAcc.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                checkLogin(Name.getText().toString(),Email.getText().toString(), Password.getText().toString());
+            }
+        });
+
+        //create admin acc button
+        CreateAdminAcc.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                String name = Name.getText().toString();
+                String password = Password.getText().toString();
+                //name and pass = admin or Admin
+                if (name.equalsIgnoreCase("admin")&& password.equalsIgnoreCase("admin")){
+                    Intent loginScreen = new Intent(MainActivity.this, LoginActivity.class);
+                    loginScreen.putExtra("ADMIN", name);
+                    loginScreen.putExtra("ROLE", userrole);
+                    startActivity(loginScreen);
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "Invalid admin", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
     }
-    //called when log in is pressed
+
+    //called when login button is pressed
     //checks username and password
-    private void checkPassword(String name, String password){
-        if((name=="Admain")&&(password=="Admain")){
-            Intent intent = new Intent(MainActivity.this, AdmainActivity.class);
-            startActivity(intent);
-        }
+    private void checkLogin(String name, String Email, String password){
+
     }
 }
