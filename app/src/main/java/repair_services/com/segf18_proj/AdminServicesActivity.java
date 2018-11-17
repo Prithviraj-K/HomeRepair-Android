f33e45af3c14d6538b3d8cf7a79f7add62a5dce1
Raj Khandekar <khandekarp@outlook.com>
2018-11-17 10:51:46 -0500

    cleanup

--- app/src/main/java/repair_services/com/segf18_proj/AdminServicesActivity.java
+++ app/src/main/java/repair_services/com/segf18_proj/AdminServicesActivity.java
@@ -1,22 +1,13 @@
 package repair_services.com.segf18_proj;
 
-import android.content.ContentProvider;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.InputType;
-import android.text.TextUtils;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
@@ -26,24 +17,14 @@ import android.widget.Toast;
 
 import com.firebase.ui.database.FirebaseListAdapter;
 import com.firebase.ui.database.FirebaseListOptions;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
-import com.google.firebase.database.ValueEventListener;
-
-import java.util.ArrayList;
 
 public class AdminServicesActivity extends AppCompatActivity {
 
-    private DatabaseReference mDatabase;
     FirebaseListAdapter<Service> adapter;
 
-
-    ArrayList<Button> aList;
     ListView serviceList;
     Button addService;
 
@@ -52,10 +33,8 @@ public class AdminServicesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_services);
 
-        mDatabase = FirebaseDatabase.getInstance().getReference("Services");
-
-        addService = (Button) findViewById(R.id.addService);
-        serviceList = (ListView) findViewById(R.id.serviceList);
+        addService = findViewById(R.id.addService);
+        serviceList = findViewById(R.id.serviceList);
 
         serviceList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
--- app/src/main/java/repair_services/com/segf18_proj/MainActivity.java
+++ app/src/main/java/repair_services/com/segf18_proj/MainActivity.java
@@ -7,7 +7,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -15,10 +14,6 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 
