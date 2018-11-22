fe02afc75ef27fcbbc5e7705da65a54549ee34e6
Tony Pei <tonypei1998@gmail.com>
2018-11-21 23:21:32 -0500

    deliverable 2 unit test cases

---
+++ app/src/test/java/repair_services/com/segf18_proj/ProfileNameValidationTest.java
@@ -0,0 +1,23 @@
+package repair_services.com.segf18_proj;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+public class ProfileNameValidationTest {
+    @Test
+    public void profileName_CorrectProfileNameSample_ReturnTrue() {
+        assertTrue(isValidName("testname"));
+    }
+
+    @Test
+    public void profileName_CorrectProfileNameSample_ReturnFalse() {
+        assertFalse(isValidName(""));
+    }
+
+    @Test
+    public void profileName_CorrectProfileNameSample_ReturnFalse() {
+        assertFalse(isValidName(".,/.,"));
+    }
+}
